fa0d96a385965eebafe2dc9143d52f670ed713ec
karim chebbi <karimchebbion@gmail.com>
2026-06-12 13:12:03 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+// require express
+const express = require("express");
+
+// create an instance of express
+const app = express();
+
+// middleare to parse JSON
+app.use(express.json());
+
+// require dotenv
+require("dotenv").config();
+
+// PORT
+const PORT = process.env.PORT || 5005;
+
+// listen on PORT
+app.listen(PORT, (error) => {
+  error
+    ? console.log("Error starting server: ", error)
+    : console.log(`⚡ ⚡ ⚡ Server is running on port http://localhost:${PORT}`);
+});
+
+// home route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// // connect to MongoDB
+// const connectDB = require("./config/connectDB");
+// connectDB();
+
+// // require car routes
+// app.use("/api/cars", require("./routes/carRoutes"));
+
+// // require auth routes
+// app.use("/api/auth", require("./routes/authRoutes"));
